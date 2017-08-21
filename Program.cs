be0f5ac3db9f4a088a85816ded3c966eedc8a49d
Dmitry Nikulin <dmitr-nikulin@mail.ru>
2017-08-21 20:06:07 +0300

    Initial commit, add simple UI

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Avalonia;
+
+namespace AvaloniaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .Start<MainWindow>();
+        }
+    }
+}
